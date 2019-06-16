d95c9166f34440d7c823777b80d35e8222ba8450
gary <>
2019-06-16 13:34:37 +0800

    import

---
+++ books/cv/get.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+
+for ((i=1; i<=16;i++))
+do
+        echo "https://courses.cs.washington.edu/courses/cse576/book/ch$i.pdf"
+        wget "https://courses.cs.washington.edu/courses/cse576/book/ch$i.pdf"
+
+done
+
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch1.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch1.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
+#wget https://courses.cs.washington.edu/courses/cse576/book/ch2.pdf
